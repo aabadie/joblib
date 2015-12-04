5e89937d192b54cd2257c71d9902870e1ee95bd8
Alexandre Abadie <alexandre.abadie@inria.fr>
2015-12-04 12:02:31 +0100

    importing numpy functions only when required

--- joblib/numpy_pickle_utils.py
+++ joblib/numpy_pickle_utils.py
@@ -32,11 +32,6 @@ def hex_str(an_int):
 try:
     import numpy as np
     from numpy.compat import asstr, asbytes
-
-    # Those globals depends on numpy import
-    MAGIC_PREFIX = asbytes('\x93NUMPY')
-    MAGIC_LEN = len(MAGIC_PREFIX) + 2
-    BUFFER_SIZE = 2**18  # size of buffer for reading npz files in bytes
 except ImportError:
     if PY3:
         def asbytes(s):
@@ -255,196 +250,199 @@ def _open_memmap(filename, array_offset=0, mode='r+', dtype=None, shape=None,
 
     return marray, offset
 
+# Import utility functions/variables from numpy required for _open_memmap.
+# We need at least the functions introduced in version 1.9 of numpy.
+if (np is not None and (int(np.version.version.split('.')[0]) > 1 or
+                           (int(np.version.version.split('.')[0]) == 1 and
+                            int(np.version.version.split('.')[1]) == 9))):
+    from numpy.lib.format import MAGIC_LEN, MAGIC_PREFIX, BUFFER_SIZE
+    from numpy.lib.format import _check_version
+    from numpy.lib.format import _read_bytes
+    from numpy.lib.format import read_magic as _read_numpy_magic
+    from numpy.lib.format import _filter_header
+    from numpy.lib.format import _read_array_header
+else:
+    # For older versions of numpy, we use the ones from numpy 1.10.
+    MAGIC_PREFIX = asbytes('\x93NUMPY')
+    MAGIC_LEN = len(MAGIC_PREFIX) + 2
+    BUFFER_SIZE = 2**18  # size of buffer for reading npz files in bytes
 
-# The following functions are imported from nympy 1.10 to support numpy
-# versions previous to 1.9
+    def _check_version(version):
+        """Utility that check if the version is supported.
 
-# difference between version 1.0 and 2.0 is a 4 byte (I) header length
-# instead of 2 bytes (H) allowing storage of large structured arrays
+        An exception is raised if version is not supported.
 
-def _check_version(version):
-    """Utility that check if the version is supported.
+        Parameters
+        ----------
+        version: tuple
+            2 element tuple containing the major and minor versions.
 
-    An exception is raised if version is not supported.
+        Raises
+        ------
+        ValueError
+            If the version is invalid.
 
-    Taken from numpy 1.10.
+        """
+        if version not in [(1, 0), (2, 0), None]:
+            msg = "we only support format version (1,0) and (2, 0), not %s"
+            raise ValueError(msg % (version,))
 
-    Parameters
-    ----------
-    version: tuple
-        2 element tuple containing the major and minor versions.
+    def _read_bytes(fp, size, error_template="ran out of data"):
+        """Read from file-like object until size bytes are read.
 
-    Raises
-    ------
-    ValueError
-        If the version is invalid.
+        Raises ValueError if not EOF is encountered before size bytes are read.
+        Non-blocking objects only supported if they derive from io objects.
 
-    """
-    if version not in [(1, 0), (2, 0), None]:
-        msg = "we only support format version (1,0) and (2, 0), not %s"
-        raise ValueError(msg % (version,))
+        Required as e.g. ZipExtFile in python 2.6 can return less data than
+        requested.
 
+        Parameters
+        ----------
+        fp: file-like object
+        size: int
+        error_template: str
 
-def _read_bytes(fp, size, error_template="ran out of data"):
-    """Read from file-like object until size bytes are read.
+        Returns
+        -------
+        a bytes object
+            The data read in bytes.
 
-    Raises ValueError if not EOF is encountered before size bytes are read.
-    Non-blocking objects only supported if they derive from io objects.
-
-    Required as e.g. ZipExtFile in python 2.6 can return less data than
-    requested.
-
-    Taken from numpy 1.10.
-
-    Parameters
-    ----------
-    fp: file-like object
-    size: int
-    error_template: str
-
-    Returns
-    -------
-    a bytes object
-        The data read in bytes.
-
-    """
-    data = bytes()
-    while True:
-        # io files (default in python3) return None or raise on
-        # would-block, python2 file will truncate, probably nothing can be
-        # done about that.  note that regular files can't be non-blocking
-        try:
-            r = fp.read(size - len(data))
-            data += r
-            if len(r) == 0 or len(data) == size:
-                break
-        except io.BlockingIOError:
-            pass
-    if len(data) != size:
-        msg = "EOF: reading %s, expected %d bytes got %d"
-        raise ValueError(msg % (error_template, size, len(data)))
-    else:
-        return data
-
-
-def _read_numpy_magic(fp):
-    """Read the magic string to get the version of the file format.
-
-    Taken from numpy 1.10.
-
-    Parameters
-    ----------
-    fp : file_like
-
-    Returns
-    -------
-    major : int
-    minor : int
-
-    """
-    magic_str = _read_bytes(fp, MAGIC_LEN, "magic string")
-    if magic_str[:-2] != MAGIC_PREFIX:
-        msg = "the magic string is not correct; expected %r, got %r"
-        raise ValueError(msg % (MAGIC_PREFIX, magic_str[:-2]))
-    if sys.version_info[0] < 3:
-        major, minor = map(ord, magic_str[-2:])
-    else:
-        major, minor = magic_str[-2:]
-    return major, minor
-
-
-def _filter_header(s):
-    """Clean up 'L' in npz header ints.
-
-    Cleans up the 'L' in strings representing integers. Needed to allow npz
-    headers produced in Python2 to be read in Python3.
-
-    Taken from numpy 1.10.
-
-    Parameters
-    ----------
-    s : byte string
-        Npy file header.
-
-    Returns
-    -------
-    header : str
-        Cleaned up header.
-
-    """
-    import tokenize
-    if sys.version_info[0] >= 3:
-        from io import StringIO
-    else:
-        from StringIO import StringIO
-
-    tokens = []
-    last_token_was_number = False
-    for token in tokenize.generate_tokens(StringIO(asstr(s)).read):
-        token_type = token[0]
-        token_string = token[1]
-        if (last_token_was_number and
-                token_type == tokenize.NAME and
-                token_string == "L"):
-            continue
+        """
+        data = bytes()
+        while True:
+            # io files (default in python3) return None or raise on
+            # would-block, python2 file will truncate, probably nothing can be
+            # done about that.  note that regular files can't be non-blocking
+            try:
+                r = fp.read(size - len(data))
+                data += r
+                if len(r) == 0 or len(data) == size:
+                    break
+            except io.BlockingIOError:
+                pass
+        if len(data) != size:
+            msg = "EOF: reading %s, expected %d bytes got %d"
+            raise ValueError(msg % (error_template, size, len(data)))
         else:
-            tokens.append(token)
-        last_token_was_number = (token_type == tokenize.NUMBER)
-    return tokenize.untokenize(tokens)
+            return data
 
+    def _read_numpy_magic(fp):
+        """Read the magic string to get the version of the file format.
 
-def _read_array_header(fp, version):
-    """Read array header in file.
+        Parameters
+        ----------
+        fp : file_like
 
-    Taken from numpy 1.10.
+        Returns
+        -------
+        major : int
+        minor : int
 
-    """
-    # Read an unsigned, little-endian short int which has the length of the
-    # header.
-    import struct
-    if version == (1, 0):
-        hlength_str = _read_bytes(fp, 2, "array header length")
-        header_length = struct.unpack('<H', hlength_str)[0]
-        header = _read_bytes(fp, header_length, "array header")
-    elif version == (2, 0):
-        hlength_str = _read_bytes(fp, 4, "array header length")
-        header_length = struct.unpack('<I', hlength_str)[0]
-        header = _read_bytes(fp, header_length, "array header")
-    else:
-        raise ValueError("Invalid version %r" % version)
+        Note
+        ----
+        Original name in numpy is read_magic.
 
-    # The header is a pretty-printed string representation of a literal
-    # Python dictionary with trailing newlines padded to a 16-byte
-    # boundary. The keys are strings.
-    #   "shape" : tuple of int
-    #   "fortran_order" : bool
-    #   "descr" : dtype.descr
-    header = _filter_header(header)
-    try:
-        d = np.safe_eval(header)
-    except SyntaxError as e:
-        msg = "Cannot parse header: %r\nException: %r"
-        raise ValueError(msg % (header, e))
-    if not isinstance(d, dict):
-        msg = "Header is not a dictionary: %r"
-        raise ValueError(msg % d)
-    keys = sorted(d.keys())
-    if keys != ['descr', 'fortran_order', 'shape']:
-        msg = "Header does not contain the correct keys: %r"
-        raise ValueError(msg % (keys,))
+        """
+        magic_str = _read_bytes(fp, MAGIC_LEN, "magic string")
+        if magic_str[:-2] != MAGIC_PREFIX:
+            msg = "the magic string is not correct; expected %r, got %r"
+            raise ValueError(msg % (MAGIC_PREFIX, magic_str[:-2]))
+        if sys.version_info[0] < 3:
+            major, minor = map(ord, magic_str[-2:])
+        else:
+            major, minor = magic_str[-2:]
+        return major, minor
 
-    # Sanity-check the values.
-    if (not isinstance(d['shape'], tuple) or
-            not np.all([isinstance(x, (int, np.long)) for x in d['shape']])):
-        msg = "shape is not valid: %r"
-        raise ValueError(msg % (d['shape'],))
-    if not isinstance(d['fortran_order'], bool):
-        msg = "fortran_order is not a valid bool: %r"
-        raise ValueError(msg % (d['fortran_order'],))
-    try:
-        dtype = np.dtype(d['descr'])
-    except TypeError as e:
-        msg = "descr is not a valid dtype descriptor: %r"
-        raise ValueError(msg % (d['descr'],))
+    def _filter_header(s):
+        """Clean up 'L' in npz header ints.
 
-    return d['shape'], d['fortran_order'], dtype
+        Cleans up the 'L' in strings representing integers. Needed to allow npz
+        headers produced in Python2 to be read in Python3.
+
+        Parameters
+        ----------
+        s : byte string
+            Npy file header.
+
+        Returns
+        -------
+        header : str
+            Cleaned up header.
+
+        """
+        import tokenize
+        if sys.version_info[0] >= 3:
+            from io import StringIO
+        else:
+            from StringIO import StringIO
+
+        tokens = []
+        last_token_was_number = False
+        for token in tokenize.generate_tokens(StringIO(asstr(s)).read):
+            token_type = token[0]
+            token_string = token[1]
+            if (last_token_was_number and
+                    token_type == tokenize.NAME and
+                    token_string == "L"):
+                continue
+            else:
+                tokens.append(token)
+            last_token_was_number = (token_type == tokenize.NUMBER)
+        return tokenize.untokenize(tokens)
+
+    def _read_array_header(fp, version):
+        """Read array header in file.
+
+        Taken from numpy 1.10.
+
+        """
+        # Read an unsigned, little-endian short int which has the length of the
+        # header.
+        import struct
+        if version == (1, 0):
+            hlength_str = _read_bytes(fp, 2, "array header length")
+            header_length = struct.unpack('<H', hlength_str)[0]
+            header = _read_bytes(fp, header_length, "array header")
+        elif version == (2, 0):
+            hlength_str = _read_bytes(fp, 4, "array header length")
+            header_length = struct.unpack('<I', hlength_str)[0]
+            header = _read_bytes(fp, header_length, "array header")
+        else:
+            raise ValueError("Invalid version %r" % version)
+
+        # The header is a pretty-printed string representation of a literal
+        # Python dictionary with trailing newlines padded to a 16-byte
+        # boundary. The keys are strings.
+        #   "shape" : tuple of int
+        #   "fortran_order" : bool
+        #   "descr" : dtype.descr
+        header = _filter_header(header)
+        try:
+            d = np.safe_eval(header)
+        except SyntaxError as e:
+            msg = "Cannot parse header: %r\nException: %r"
+            raise ValueError(msg % (header, e))
+        if not isinstance(d, dict):
+            msg = "Header is not a dictionary: %r"
+            raise ValueError(msg % d)
+        keys = sorted(d.keys())
+        if keys != ['descr', 'fortran_order', 'shape']:
+            msg = "Header does not contain the correct keys: %r"
+            raise ValueError(msg % (keys,))
+
+        # Sanity-check the values.
+        if (not isinstance(d['shape'], tuple) or
+                not np.all([isinstance(x, (int, np.long))
+                            for x in d['shape']])):
+            msg = "shape is not valid: %r"
+            raise ValueError(msg % (d['shape'],))
+        if not isinstance(d['fortran_order'], bool):
+            msg = "fortran_order is not a valid bool: %r"
+            raise ValueError(msg % (d['fortran_order'],))
+        try:
+            dtype = np.dtype(d['descr'])
+        except TypeError as e:
+            msg = "descr is not a valid dtype descriptor: %r"
+            raise ValueError(msg % (d['descr'],))
+
+        return d['shape'], d['fortran_order'], dtype
